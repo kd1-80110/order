88eb8675d98d70f0665a5bed655d213ae2712baf
kd1-80110 <144333139+kd1-80110@users.noreply.github.com>
2024-08-06 17:13:17 +0530

    Delete customer-hotel-delivery-service directory

--- customer-hotel-delivery-service/src/main/java/com/app/dao/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entity.Customer;
-
-public interface CustomerDao extends JpaRepository<Customer, String> {
-
-}
